51a079c8b3795fa9ad8798149886869bd0e88ddd
amscad <ajmansfield@gmail.com>
2024-03-26 21:12:45 +0000

    Use Brew for managing asdf now it is mature, and ensure its available

--- bin/asdfvm.sh
+++ bin/asdfvm.sh
@@ -2,7 +2,7 @@
 # PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/MacGPG2/bin
 #https://stackoverflow.com/questions/192292/bash-how-best-to-include-other-scripts
 
-# Get the config path - used to include other files 
+# Get the config path - used to include other files
 SCRIPT_PATH=$(dirname "$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")")
 PARENT_PATH=$(dirname "$SCRIPT_PATH")
 CONFIG_PATH="$PARENT_PATH/config"
@@ -11,12 +11,13 @@ source "$BIN_PATH/functions.sh"
 
 
 ###################################[ version manager ]########################
-echo "Configuring asdf version manager..."
-if [ ! -d "$HOME/.asdf" ]; then
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf 
-  append_to_zshrc "source $HOME/.asdf/asdf.sh" 1
-  append_to_zshrc "source $HOME/.asdf/completions/asdf.bash" 1
-fi
+source "$HOME/.zshrc"
+# echo "Configuring asdf version manager..."
+# if [ ! -d "$HOME/.asdf" ]; then
+#   git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+#   append_to_zshrc "source $HOME/.asdf/asdf.sh" 1
+#   append_to_zshrc "source $HOME/.asdf/completions/asdf.bash" 1
+# fi
 
 install_asdf_plugin() {
   local name="$1"
@@ -42,7 +43,7 @@ install_asdf_language() {
   if [ "$version" == "latest" ]; then
     version="$(asdf list-all "$language" | grep -v "[a-z]" | tail -1)"
   fi
-  
+
   echo "Installing $language $version"
 
   if ! asdf list "$language" | grep -Fq "$version"; then
